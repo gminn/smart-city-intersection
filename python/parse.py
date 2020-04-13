68ea1e4f55cb74f499bce4d6eca511f11e1b896d
gminn <gminn@umich.edu>
2020-04-13 11:08:25 -0400

    Fix bug in get_intersection_objs

--- python/parse.py
+++ python/parse.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-# TODO define traffic directions
-from argparse import ArgumentParser
 from pathlib import Path
+from argparse import ArgumentParser
 from output_pb2 import OutputMessage
 import sys
 
@@ -36,6 +35,39 @@ def label_to_string(label):
         return 'Cyclist'
     return 'Misc'
 
+def zone_to_Lane(zone):
+   
+    tr_direct = ''
+    is_left_turn = False
+    if zone.id == 0:
+        tr_direct = 'None'
+        is_left_turn = False
+    elif zone.id == 1: # Northbound, straight
+        tr_direct = 'North'
+        is_left_turn = False
+    elif zone.id == 2: # Northbound, left
+        tr_direct = 'North'
+        is_left_turn = True
+    elif zone.id == 3: # Southbound, straight
+        tr_direct = 'South'
+        is_left_turn = False
+    elif zone.id == 4: # Southbound, left
+        tr_direct = 'South'
+        is_left_turn = True
+    elif zone.id == 5: # Eastbound, straight
+        tr_direct = 'East'
+        is_left_turn = False
+    elif zone.id == 6: # Eastbound, left
+        tr_direct = 'East'
+        is_left_turn = True
+    elif zone.id == 7: # Westbound, straight
+        tr_direct = 'West'
+        is_left_turn = False
+    elif zone.id == 8: # Westbound, left
+        tr_direct = 'West'
+        is_left_turn = True
+    return Lane(tr_direct, is_left_turn)
+
 # returns reqested lane has been configured
 def is_vaild_lane(lanes_arr, zone_id):
     for lane in lanes_arr:
@@ -51,18 +83,20 @@ def parse_output_file(filename):
         return output
 
 # returns whether an object is in a particular lane
-def is_in_lane(output, traffic, obj_id, zone_id):
-    if ~is_valid_lane(output, zone_id):
+def is_in_lane(output, lanes_arr, obj_id, zone_id):
+    if ~is_valid_lane(lanes_arr, zone_id): # check if valid lane
         sys.exit("Error: Invalid lane entry. Lane does not exist. Exiting.")
     else:
-        for zone in output.region_of_interest:
+        for zone in output.region_of_interest: # iterate over configured regions
             if(zone.id == zone_id):
-                for obj in zone.object_id:
-                    if(zone.object_ids == obj_id):
+                for obj in zone.object_ids:
+                    if(obj.id== obj_id):
                         return True
         return False
 
 # returns a list populated with lane information
+# parameter output is the translated binary output (return 
+# value of the function parse_output_file()
 def populate_lanes(output):
     try:
         regions = output.region_of_interest
@@ -71,11 +105,8 @@ def populate_lanes(output):
     lanes_arr = []
     for zone in output.region_of_interest:
         
-        # TODO: figure out how to know which ids correspond to
-        # which lane directions
-        
-        # TODO: find a more elegant way to assign member vars
-        # and populate my list of lanes
+        # TODO: change which ids correspond to
+        # which lane directions given test data
         tr_direct = ''
         is_left_turn = False
         if zone.id == 0:
@@ -119,14 +150,17 @@ def is_reliable_tracking(object):
         print(f'Tracking reliable field not set for Object #{object.id}')
         return -1
 
-def get_intersection_objs():
+# returns matrix of objects
+# parameter bag_dir is a path to a directory with bin files
+# from a ROS bag
+def get_intersection_objs(bag_dir):
     p = Path('../binary_files')
     if(p.exists() is not True):
         sys.exit("Error: No ../binary_files folder.")
     bin_paths = []
     objs_matrix = []
     for dir in p.iterdir():
-        if dir.is_dir():
+        if str(dir) == bag_dir:
             row = []
             bin_paths = list(p.glob('**/*.bin')) # gen list of paths
             for bin_file in bin_paths: # outer loop
@@ -140,14 +174,17 @@ def get_intersection_objs():
                             obj.bbox.position.y,
                             obj.track.velocity.x,
                             obj.track.velocity.y,
-                            label
-                            )
-                            )
+                            label))
                 objs_matrix.append(row)
     return objs_matrix
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('filename', type=str)
+    args = parser.parse_args()
     
+    object_matrix = get_intersection_objs(args.filename)
+    print(object_matrix)
 
 if __name__ == '__main__':
     main();
